2864f079ef6dd6b45d64508550df7f1a19bd16ea
İ Ayşe Kaya <i.ayse-kaya@hotmail.com>
2023-01-27 15:12:09 +0300

    before add api / added card-modal

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {CardModalComponent} from "./card-modal/card-modal.component";
 
 @Component({
   selector: 'app-cards',
@@ -10,12 +12,19 @@ export class CardsComponent implements OnInit{
 cardItem = {
   title :'first card',
   name : 'ayşe',
-  tel : '0505000000',
+  phone : '0505000000',
   email :'example@gmail.com',
   address:'somewhere',
 };
-constructor() {
-}
+constructor(
+  public dialog:MatDialog
+) {}
 ngOnInit() : void {
+}
+
+openAddCardModal():void{
+  this.dialog.open(CardModalComponent,
+    {width: '800px'});
+
 }
 }
